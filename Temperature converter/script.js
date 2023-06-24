f927642d3fe098899523c79a37601bd9df0d3e76
shubhamshelke29 <137604801+shubhamshelke29@users.noreply.github.com>
2023-06-24 13:45:00 +0530

    Add files via upload

---
+++ Temperature converter/script.js
@@ -0,0 +1,11 @@
+function convertToFar() {
+  var celsius = document.getElementById("celsius").value;
+  var fahrenheit = (celsius * 9/5) + 32;
+  document.getElementById("fahrenheit").value = fahrenheit;
+}
+
+function convertToCel() {
+  var fahrenheit = document.getElementById("fahrenheit").value;
+  var celsius = (fahrenheit - 32) * 5/9;
+  document.getElementById("celsius").value = celsius;
+}
